93ccc5fdb81ae8f5afb6583ff392a58ac05f2232
Travis Hoppe <travis.hoppe@gmail.com>
2017-02-08 13:50:51 -0500

    Add shuffle option to embeding

--- w2v_pipeline/model_building/d2v_embedding.py
+++ w2v_pipeline/model_building/d2v_embedding.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 
 import gensim.models
 import psutil
+from random import shuffle
 
 CPU_CORES = 1 #psutil.cpu_count()
 assert (gensim.models.doc2vec.FAST_VERSION > -1)
@@ -26,27 +27,30 @@ class d2v_embedding(corpus_iterator):
         )
 
         self.data = None
-
+        self.shuffle = kwargs["shuffle_during_epochs"]
 
     def preload_dataset(self, **config):
         print("Preloading datset into memory.")
         LSI = self.labelized_sentence_iterator
         self.data = list(LSI(config["target_column"]))
 
+    def get_data(self):
+        if self.data is None:
+            raise NotImplementedError("For now, data must be preloaded")
+
+        if self.shuffle:
+            shuffle(self.data)
+        
+        return self.data
+
     def compute(self, **config):
         print("Learning the vocabulary")
 
-        if self.data is None:
-            raise NotImplementedError("For now, data must be preloaded")
-            #LSI = self.labelized_sentence_iterator       
-            #ITR = LSI(config["target_column"])
-            #self.clf.build_vocab(ITR)
-
-        self.clf.build_vocab(self.data)
+        self.clf.build_vocab(self.get_data())
 
         print("Training the features")
         for n in tqdm(range(self.epoch_n)):
-            self.clf.train(self.data)
+            self.clf.train(self.get_data())
 
         print("Reducing the features")
         self.clf.init_sims(replace=True)
