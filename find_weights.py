90ed8f801d223b2e9d652f3fa24f63979ec86809
Travis Hoppe <travis.hoppe@gmail.com>
2017-03-22 01:24:42 -0400

    Primitive test

--- find_weights.py
+++ find_weights.py
@@ -1,5 +1,7 @@
 import numpy as np
 from tqdm import tqdm
+from sklearn.preprocessing import normalize
+from scipy.spatial.distance import pdist
 
 from word2vec_pipeline.utils.data_utils import load_w2vec
 from word2vec_pipeline.utils.db_utils import item_iterator
@@ -20,9 +22,25 @@ for item in tqdm(item_iterator()):
             continue
         row[word2index[w]] += 1
     V.append(row)
+    if len(V)>10: break
 
 V = np.array(V)
+#print V.shape
+#print V
+v0 = V[0]
 
-print V.shape
 
-print V
+def objective_func(alpha):
+    X = normalize((V*alpha).dot(W.syn0))
+    dist = pdist(X, metric='cosine')
+    print dist.sum(),alpha.max(), alpha.min()
+    return dist.sum()
+
+from scipy.optimize import minimize, fmin
+
+alpha = np.ones(n_vocab)
+
+# Starts off at 31.19, 11.28
+print minimize(objective_func, alpha)#, method='Nelder-Mead')
+
+
